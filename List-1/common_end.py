43708a06dd38bb6204a1deaa45673d64968aa939
NinjaGrandmaster <arenas1944@gmail.com>
2021-11-08 15:09:32 -0800

    Completed common end exercise

---
+++ List-1/common_end.py
@@ -0,0 +1,25 @@
+# Given 2 arrays of ints, a and b, return True if they have the same first element or
+# they have the same last element. Both arrays will be length 1 or more.
+#
+# common_end([1, 2, 3], [7, 3]) → True
+# common_end([1, 2, 3], [7, 3, 2]) → False
+# common_end([1, 2, 3], [1, 3]) → True
+def common_end(a, b):
+    # check first element
+    if a[0] == b[0]:
+        return True
+    # check last element
+    if a[len(a) - 1] == b[len(b) - 1]:
+        return True
+
+    return False
+
+
+def main():
+    print(common_end([1, 2, 3], [7, 3]))
+    print(common_end([1, 2, 3], [7, 3, 2]))
+    print(common_end([1, 2, 3], [1, 3]))
+
+
+if __name__ == '__main__':
+    main()
