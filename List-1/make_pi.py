7d2ca21912e284f7c2f516e6b9c470cfd10d95dd
NinjaGrandmaster <arenas1944@gmail.com>
2021-11-08 15:05:09 -0800

    Completed make pi exercise

---
+++ List-1/make_pi.py
@@ -0,0 +1,13 @@
+# Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
+#
+# make_pi() → [3, 1, 4]
+def make_pi():
+    return [3, 1, 4]
+
+
+def main():
+    print(make_pi())
+
+
+if __name__ == '__main__':
+    main()
