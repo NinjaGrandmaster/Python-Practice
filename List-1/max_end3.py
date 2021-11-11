4b3f5d2d45f7788e23853e1329399925df3a15af
NinjaGrandmaster <arenas1944@gmail.com>
2021-11-10 17:23:15 -0800

    Completed max end 3 exercise

---
+++ List-1/max_end3.py
@@ -0,0 +1,27 @@
+# Given an array of ints length 3, figure out which is larger, the first or last element in the array,
+# and set all the other elements to be that value. Return the changed array.
+#
+# max_end3([1, 2, 3]) → [3, 3, 3]
+# max_end3([11, 5, 9]) → [11, 11, 11]
+# max_end3([2, 11, 3]) → [3, 3, 3]
+def max_end3(nums):
+
+    if nums[0] > nums[len(nums) - 1]:
+        max_val = nums[0]
+    else:
+        max_val = nums[len(nums) - 1]
+
+    for i in range(len(nums)):
+        nums[i] = max_val
+
+    return nums
+
+
+def main():
+    print(max_end3([1, 2, 3]))
+    print(max_end3([11, 5, 9]))
+    print(max_end3([2, 11, 3]))
+
+
+if __name__ == '__main__':
+    main()
