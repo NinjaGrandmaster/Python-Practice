86a1ee4bc54ba5fb44c5b31e1d03daf993af1d5a
NinjaGrandmaster <arenas1944@gmail.com>
2021-11-15 18:48:58 -0800

    Completed is palindrome problem without string conversion

--- LeetCode-Easy/ispalindrome.py
+++ LeetCode-Easy/ispalindrome.py
@@ -11,13 +11,38 @@ def is_palindrome(x):
     return False
 
 
+# is palindrome function with out string conversion
+def is_palindrome_nc(x):
+    # negative numbers cannot be palindromes due to "-" symbol
+    if x < 0:
+        return False
+
+    temp_num = x
+    reversed_num = 0
+    # reverse number
+    while temp_num > 0:
+        reversed_num = (reversed_num * 10) + (temp_num % 10)
+        temp_num = temp_num // 10  # perform integer (floor) division
+
+    if x == reversed_num:
+        return True
+
+    return False
+
+
 def main():
     print(is_palindrome(121))  # true
     print(is_palindrome(-121))  # false
     print(is_palindrome(10))  # false
     print(is_palindrome(-101))  # true
 
+    print()
+
+    print(is_palindrome_nc(121))  # true
+    print(is_palindrome_nc(-121))  # false
+    print(is_palindrome_nc(10))  # false
+    print(is_palindrome_nc(-101))  # true
+
 
 if __name__ == '__main__':
     main()
-
